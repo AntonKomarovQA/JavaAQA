5a8029fe56d3b9bb10414d07c9ae57a2874bb033
Anton K <komarov@firecode.ru>
2024-06-29 15:09:19 +0300

    Eddit class DOG mothod selectRandom case5 and method comeDoge

--- src/Practika/Dog.java
+++ src/Practika/Dog.java
@@ -10,25 +10,33 @@ public class Dog {
     private int age;
     private DogType type;
 
-    static void lay(){
+    static void lay() {
         System.out.println("Gav Gav");
     }
-    static void relax(){
+
+    static void relax() {
         System.out.println("Spit");
     }
-    static void kus(){
+
+    static void kus() {
         System.out.println("Kusaet");
     }
-    static void run(){
+
+    static void run() {
         System.out.println("Beg");
     }
+
     //Собака убивается
-    static void rip(List<Dog> dog, Dog dogs){
-        System.out.println("ymer pes");
+    static void rip(List<Dog> dog, Dog dogs) {
         dog.remove(dogs);
+        System.out.println("Ymer pes" + " Осталось собак " + dog.size());
     }
-    static void comeDog(){
-        System.out.println("Pes +");
+
+    //Пополнение собак на одну
+    static void comeDog(List<Dog> dog) {
+        Random random = new Random();
+        dog.add(new Dog((getRandomName(random)), getRandomAge(random), getRandonType(random)));
+        System.out.println("Pes + " +"Пополнение в стае собак "+dog.size());
     }
 
     public Dog(String name, int age, DogType type) {
@@ -38,46 +46,53 @@ public class Dog {
 
     }
 
-     @Override
-     public String toString() {
-         return "Dog " + "name= " + name + ", age= " + age +
-                 ", type= " + type  ;
-     }
+    @Override
+    public String toString() {
+        return "Dog " + "name= " + name + ", age= " + age +
+                ", type= " + type;
+    }
 
     // Метод для получения случайного name собаки
-    protected static String getRandomName(Random random){
-        String[] types = { "Bob", "Lak", "Kes","Buch","Ice"};
+    protected static String getRandomName(Random random) {
+        String[] types = {"Bob", "Lak", "Kes", "Buch", "Ice"};
         return types[random.nextInt(types.length)];
     }
+
+    // Метод для получения случайного age
+    protected static int getRandomAge(Random random) {
+        return random.nextInt(12);
+    }
+
     // Метод для получения случайного типа собаки
-    protected static DogType getRandonType(Random random){
-        String[] types = { "LAIKA", "PITBUL", "HASKI", "KORGI"};
+    protected static DogType getRandonType(Random random) {
+        String[] types = {"LAIKA", "PITBUL", "HASKI", "KORGI"};
         return DogType.valueOf(types[random.nextInt(types.length)]);
     }
+
     // Метод для случайного действия
-    static void selectRandom(List<Dog> dog){
+    static void selectRandom(List<Dog> dog) {
         Random random = new Random();
         int actionIndex = random.nextInt(6);
 
-        switch (actionIndex){
+        switch (actionIndex) {
             case 0:
                 Dog.lay();
                 break;
-                case 1:
+            case 1:
                 Dog.relax();
                 break;
-                case 2:
+            case 2:
                 Dog.kus();
                 break;
-                case 3:
+            case 3:
                 Dog.run();
                 break;
-                case 4:
+            case 4:
                 Dog.rip(dog, dog.get(random.nextInt(dog.size())));
                 break;
-                case 5:
-                Dog.comeDog();
+            case 5:
+                Dog.comeDog(dog);
                 break;
         }
     }
- }
+}
--- src/Practika/Test.java
+++ src/Practika/Test.java
@@ -11,31 +11,43 @@ public class Test {
         Random random = new Random();
 
         // Создание и добавление 5 случайных объектов Dog в список
-        for (int i =0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             String name = Dog.getRandomName(random); // случайное имя
-            int age = random.nextInt(10);
+            int age = Dog.getRandomAge(random);// случайный age
             DogType type = Dog.getRandonType(random); // случайный тип
-            dogs.add(new Dog(name,age,type));
+            dogs.add(new Dog(name, age, type));
         }
-        // Вывод содержимого списка
-        for (Dog dog : dogs){
-            System.out.println(dog);
-        }
+        int colvoDog = dogs.size();
+//        // Вывод содержимого списка
+//        for (Dog dog : dogs) {
+//            System.out.println(dog);
+//        }
 
 
         final int dayYear = 365;
         final int dayNedela = 7;
 
         // в течении 1 года выбирается случайная собака и выполняется случайное действие
-        for (int day = 1; day<dayYear; day++){
-            if (day % dayNedela == 0){
-                if (dogs.isEmpty()){
+        for (int day = 1; day < dayYear; day++) {
+            if (day % dayNedela == 0) {
+                if (dogs.isEmpty()) {
                     System.out.println("Все собаки умерли");
                     break;
                 }
                 Dog.selectRandom(dogs);
-
+            }
+            //Каждый 30 день пополняется 1 собака
+            if (day % 30 == 0) {
+                Dog.comeDog(dogs);
             }
         }
+
+//        // Вывод нового списка собак
+//        for (Dog dog : dogs) {
+//            System.out.println(dog);
+//        }
+
+
+        System.out.println("Количество стаи собак в начале года "+colvoDog + " Спустя год " + dogs.size());
     }
 }
